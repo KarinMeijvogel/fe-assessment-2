68d8865a543f3803e83740a6928df1fe29a33f7e
karinmeijvogel <karinmeijvogel@hotmail.com>
2020-04-11 00:24:27 +0200

    :children_crossing: Added /reset page to reset the local storage to the default user data

--- scripts/likedpage.js
+++ scripts/likedpage.js
@@ -1,27 +1,3 @@
-// default userdata
-class Person{
-    constructor(firstName, lastName, age, desc, photo, msg, liked, likedMe) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.desc = desc;
-        this.photo = photo;
-        this.msg = msg;
-        this.liked = liked;
-        this.likedMe = likedMe; // true, false or null (not liked or disliked yet)
-    }
-}
-
-let Olivia = new Person("Olivia", "Delroy", "24", "Hi, my name is Olivia. I'm a 24 year old student from London. Let's talk!", "girl.jpeg", `I love all of the bands in your...`, null, null);
-let Kayla = new Person("Kayla", "Solomon", "25", "Hi, my name is Kayla. I'm a 25 year old student from London. Let's talk!", "girl1.jpeg", `What"s up :-)`, null, null);
-let Nadia = new Person("Nadia", "Williams", "23", "Hi, my name is Nadia. I'm a 23 year old student from London. Let's talk!", "girl2.jpeg", `lol ikr`, null, null);
-let Eve = new Person("Eve", "Johnson", "24", "Hi, my name is Eve. I'm a 24 year old student from London. Let's talk!", "girl3.jpeg", `Are you going to DLDK this year?`, null, null);
-let Abby = new Person("Abby", "Watts", "25", "Hi, my name is Abby. I'm a 25 year old student from London. Let's talk!", "girl4.jpeg", `Yeah, saw them live 3 weeks ago.`, null, null);
-let defaultData = [Olivia, Kayla, Nadia, Eve, Abby];
-
-// (re)store default userdata in localStorage:
-localStorage.setItem("data", JSON.stringify(defaultData));
-
 // get myLikes from localStorage, divide them in matching and pending
 let data = JSON.parse(localStorage.data);
 
