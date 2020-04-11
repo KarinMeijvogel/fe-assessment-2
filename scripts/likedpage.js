968c86e05a431f326b55689a93af625c9cef0a6f
karinmeijvogel <karinmeijvogel@hotmail.com>
2020-04-11 10:13:39 +0200

    :sparkles: New matches now have a green notification next to them, until you open the chat

--- scripts/likedpage.js
+++ scripts/likedpage.js
@@ -1,17 +1,19 @@
-// get myLikes (all likes) and matches from localStorage
+// get myLikes (all likes) and matches from localStorage -------------------------------------------------------------------
 let matches = JSON.parse(localStorage.data).filter(person => {
     return person.liked == true && person.likedMe == true;
 });
 
-// html page elements
+// html page elements ------------------------------------------------------------------------------------------------------
 const main = document.getElementsByTagName("main")[0];
 const footerText = document.getElementsByTagName("footer")[0].getElementsByTagName("p")[0];
 const removeButtons = document.getElementsByTagName("button");
 const chatList = document.getElementsByTagName("ul")[0];
+const chatListItems = document.getElementsByTagName("li");
 
-// disable html-only content if js is enabled
+// disable html-only content if js is enabled -------------------------------------------------------------------------------
 chatList.innerHTML = "";
 
+// on pageload  -------------------------------------------------------------------------------------------------------------
 // if liked page is empty, hide the main (background)
 if (matches.length == 0) {
     main.classList.add("invisible");
@@ -23,18 +25,26 @@ if (matches.length == 0) {
 for (let i = 0; i < matches.length; i++) {
     
     // make a list item and put it in the "clItems" array
-    chatList.innerHTML += "<li><figure data-id='" + matches[i].firstName + "'><form action='' method='post'><button><img src='' alt='profilepicture'></button><button>x</button></form><figcaption><h4>Username</h4><p>Message</p></figcaption></figure></li>"
+    chatList.innerHTML += "<li><figure data-id='" + matches[i].firstName + "'><form action='' method='post'><button><img src='' alt='profilepicture'></button><button>x</button></form><div class='notification'></div><figcaption><h4>Username</h4><p>Message</p></figcaption></figure></li>"
     
     // give every chat the right details
     const photo = chatList.getElementsByTagName("img")[i];
     const name = chatList.getElementsByTagName("h4")[i];
     const msg = chatList.getElementsByTagName("p")[i];
+    const notification = chatList.querySelectorAll(".notification")[i];
     
     photo.src = "images/" + matches[i].photo;
     name.textContent = matches[i].firstName + " " + matches[i].lastName;
     msg.textContent = matches[i].msg;
+    
+    // show only notifications of unseen matches
+    if (matches[i].newMatch == false) {
+        notification.style.display = "none"
+    }
 }
 
+
+// functions -----------------------------------------------------------------------------------------------------------------
 // function that gets executed to delete someone from my matches
 function dislike(e) {
     e.preventDefault();
@@ -51,12 +61,34 @@ function dislike(e) {
     location.reload();
 }
 
-// removebuttons eventlistener
+// function that gets executed when user clicks on chat (to remove 'new' notification)
+function removeNotification(e) {
+    // update the currentData
+    const currentData = JSON.parse(localStorage.data);
+    const clickedUser = currentData.find(person => {
+        return person.firstName == e.target.closest("figure").dataset.id;
+    });
+
+    clickedUser.newMatch = false;
+
+    // put the updated currentData in localStorage
+    localStorage.setItem("data", JSON.stringify(currentData));
+    location.reload()
+}
+
+// eventlisteners -------------------------------------------------------------------------------------------------------------
+// ... for removebuttons
 for (i = 0; i < removeButtons.length; i++) {
     removeButtons[i].addEventListener("click", dislike)
 }
 
-// after 3 seconds, the latest user i've liked also liked me! (match effect)
+// ... for clicking on chat
+for (i = 0; i < chatListItems.length; i++) {
+    chatListItems[i].addEventListener("click", removeNotification)
+}
+
+// after 2 seconds, the latest user i've liked also liked me! (match effect) --------------------------------------------------
+setTimeout(newMatch, 2000);
 function newMatch(){
     // update the currentData
     const currentData = JSON.parse(localStorage.data);
@@ -64,11 +96,12 @@ function newMatch(){
         return person.liked === true && person.likedMe === null;
     });
     
-    newMatch.likedMe = true;
+    if (newMatch) {
+        newMatch.likedMe = true;
 
-    // put the updated currentData in localStorage
-    localStorage.setItem("data", JSON.stringify(currentData));
-    location.reload();
+        // put the updated currentData in localStorage
+        localStorage.setItem("data", JSON.stringify(currentData));
+        location.reload();    
+    }
 }
 
-setTimeout(newMatch, 3000);
--- scripts/reset.js
+++ scripts/reset.js
@@ -1,6 +1,6 @@
 // default userdata
 class Person{
-    constructor(firstName, lastName, age, desc, photo, msg, liked, likedMe) {
+    constructor(firstName, lastName, age, desc, photo, msg, liked, likedMe, newMatch) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
@@ -9,14 +9,15 @@ class Person{
         this.msg = msg;
         this.liked = liked;
         this.likedMe = likedMe; // true, false or null (not liked or disliked yet)
+        this.newMatch = newMatch
     }
 }
 
-let Olivia = new Person("Olivia", "Delroy", "24", "Hi, my name is Olivia. I'm a 24 year old student from London. Let's talk!", "girl.jpeg", `I love all of the bands in your...`, null, null);
-let Kayla = new Person("Kayla", "Solomon", "25", "Hi, my name is Kayla. I'm a 25 year old student from London. Let's talk!", "girl1.jpeg", `What's up :-)`, null, null);
-let Nadia = new Person("Nadia", "Williams", "23", "Hi, my name is Nadia. I'm a 23 year old student from London. Let's talk!", "girl2.jpeg", `lol ikr`, null, null);
-let Eve = new Person("Eve", "Johnson", "24", "Hi, my name is Eve. I'm a 24 year old student from London. Let's talk!", "girl3.jpeg", `Are you going to DLDK this year?`, null, true);
-let Abby = new Person("Abby", "Watts", "25", "Hi, my name is Abby. I'm a 25 year old student from London. Let's talk!", "girl4.jpeg", `Yeah, saw them live 3 weeks ago.`, null, true);
+let Olivia = new Person("Olivia", "Delroy", "24", "Hi, my name is Olivia. I'm a 24 year old student from London. Let's talk!", "girl.jpeg", `I love all of the bands in your...`, null, null, false);
+let Kayla = new Person("Kayla", "Solomon", "25", "Hi, my name is Kayla. I'm a 25 year old student from London. Let's talk!", "girl1.jpeg", `What's up :-)`, null, null, false);
+let Nadia = new Person("Nadia", "Williams", "23", "Hi, my name is Nadia. I'm a 23 year old student from London. Let's talk!", "girl2.jpeg", `lol ikr`, null, null, false);
+let Eve = new Person("Eve", "Johnson", "24", "Hi, my name is Eve. I'm a 24 year old student from London. Let's talk!", "girl3.jpeg", `Are you going to DLDK this year?`, null, true, false);
+let Abby = new Person("Abby", "Watts", "25", "Hi, my name is Abby. I'm a 25 year old student from London. Let's talk!", "girl4.jpeg", `Yeah, saw them live 3 weeks ago.`, null, true, false);
 let defaultData = [Olivia, Kayla, Nadia, Eve, Abby];
 
 // (re)store default userdata in localStorage:
