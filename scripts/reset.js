b9759fb1db7737ed3499cd9b0ea72f7f942cb4b2
Karin Meijvogel <karinmeijvogel@hotmail.com>
2020-04-12 13:54:38 +0200

    :bug: Fixed some minor bugs and text on reset.html

--- scripts/reset.js
+++ scripts/reset.js
@@ -14,9 +14,9 @@ class Person{
 }
 
 let Olivia = new Person("Olivia", "Delroy", "24", "Hi, my name is Olivia. I'm a 24 year old student from London. Let's talk!", "girl.jpeg", `I love all of the bands in your...`, null, null, true);
-let Kayla = new Person("Kayla", "Solomon", "25", "Hi, my name is Kayla. I'm a 25 year old student from London. Let's talk!", "girl1.jpeg", `What's up :-)`, null, null, true);
+let Kayla = new Person("Kayla", "Solomon", "25", "Hi, my name is Kayla. I'm a 25 year old student from London. Let's talk!", "girl1.jpeg", `What's up :-)`, null, true, true);
 let Nadia = new Person("Nadia", "Williams", "23", "Hi, my name is Nadia. I'm a 23 year old student from London. Let's talk!", "girl2.jpeg", `lol ikr`, null, null, true);
-let Eve = new Person("Eve", "Johnson", "24", "Hi, my name is Eve. I'm a 24 year old student from London. Let's talk!", "girl3.jpeg", `Are you going to DLDK this year?`, null, true, true);
+let Eve = new Person("Eve", "Johnson", "24", "Hi, my name is Eve. I'm a 24 year old student from London. Let's talk!", "girl3.jpeg", `Are you going to DLDK this year?`, null, null, true);
 let Abby = new Person("Abby", "Watts", "25", "Hi, my name is Abby. I'm a 25 year old student from London. Let's talk!", "girl4.jpeg", `Yeah, saw them live 3 weeks ago.`, null, true, true);
 let defaultData = [Olivia, Kayla, Nadia, Eve, Abby];
 
